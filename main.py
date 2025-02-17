5b1df65fd3e220451210f3a140d0316b647fc537
Féréol Gbenou <gbenoufereol@yahoo.com>
2025-02-17 16:57:18 +0100

    fix docker compose db connection

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ import os
 app = FastAPI()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/mydatabase")
-client = MongoClient(MONGO_URI)
-db = client.mydatabase
-collection = db.mycollection
 
-@app.post("/")
+client = MongoClient(MONGO_URI)
+db = client["dpe_database"]
+collection = db["test_collection"]
+
+@app.get("/")
 async def start():
     """Ajoute un document dans MongoDB."""
-    return {"MSG": "Hello from api module"}
+    return {"MSG": f"Hello from api module. Mongo db running on : {MONGO_URI}"}
 
-@app.post("/write/")
+@app.get("/init-db")
+async def init_db():
+    """Insère un document test pour s'assurer que la base et la collection existent."""
+    sample_doc = {"name": "Test", "value": 42}
+    collection.insert_one(sample_doc)
+    return {"message": "Database and collection initialized !"}
+
+@app.post("/write")
 async def write_data(data: dict):
     """Ajoute un document dans MongoDB."""
-    result = collection.insert_one(data)
-    return {"inserted_id": str(result.inserted_id)}
+    collection.insert_one(data)
+    return {"res": f"( {len(data)} ) elts inserted !"}
 
-@app.get("/read/")
+@app.get("/read")
 async def read_data():
     """Récupère tous les documents de MongoDB."""
-    data = list(collection.find({}, {"_id": 0}))  # exclure l'ID MongoDB
+    data = list(collection.find({}, {"_id": 0}))  # exclure l'ID mongo
     return {"data": data}
