7a480e34a46e24a6c6e1aa4508983ae4b2c6e89f
Féréol Gbenou <54837178+fereol023@users.noreply.github.com>
2025-02-18 16:57:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,53 @@ import os
 
 app = FastAPI()
 
-MONGO_URI = "mongodb://localhost:27017/mydatabase" if False else "mongodb://mongo:27017" # if config = local
+config = {
+    "ENV": os.getenv("ENVIRONMENT", "PROD")
+    "DB_HOST": os.getenv("DB_HOST", None),
+    "DB_PORT": int(os.getenv("DB_PORT", 27017)),
+    "DB_USER": os.getenv("DB_USER", None),
+    "DB_PWD": os.getenv("DB_PWD", None),
+    "DB_NAME": os.getenv("DB_NAME", "dpe_database")
+}
 
-client = MongoClient(MONGO_URI)
-db = client["dpe_database"]
-collection = db["test_collection"]
+print(config)
+
+if config.get("ENV")=="PROD":
+    client = MongoClient(
+        host=config.get('DB_HOST'), 
+        port=config.get('DB_PORT'), 
+        username=config.get('DB_USER'), 
+        password=config.get('DB_PWD')
+        )
+else:
+    client = MongoClient(
+        host="mongodb://localhost", 
+        port=config.get('DB_PORT')
+        )
+
+db = client[config.get("DB_NAME")]
+get_collection = lambda collection_name: db["collection_name"]
 
 @app.get("/")
 async def start():
-    """Ajoute un document dans MongoDB."""
-    return {"MSG": f"Hello from api module. Mongo db running on : {MONGO_URI}"}
+    """Entry point"""
+    return {"res": f"Hello from api module.", "config": ""}
 
-@app.get("/init-db")
-async def init_db():
+@app.get("/init/db/{collection}")
+async def init_db(collection: str):
     """Insère un document test pour s'assurer que la base et la collection existent."""
     sample_doc = {"name": "Test", "value": 42}
-    collection.insert_one(sample_doc)
-    return {"message": "Database and collection initialized !"}
+    get_collection(collection).insert_one(sample_doc)
+    return {"res": "Database and collection initialized !"}
 
-@app.post("/write")
-async def write_data(data: dict):
+@app.post("/write/db/{collection}")
+async def write_data(data: dict, collection: str):
     """Ajoute un document dans MongoDB."""
-    collection.insert_one(data)
+    get_collection(collection).insert_one(data)
     return {"res": f"( {len(data)} ) elts inserted !"}
 
-@app.get("/read")
-async def read_data():
+@app.get("/read/db/{collection}")
+async def read_data(collection: str):
     """Récupère tous les documents de MongoDB."""
-    data = list(collection.find({}, {"_id": 0}))  # exclure l'ID mongo
-    return {"data": data}
+    data = list(get_collection(collection).find({}, {"_id": 0}))  # exclure l'ID mongo
+    return {"res": {"data": data}}
